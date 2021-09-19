fdccfe83fc28c594b128bca23a62b0d9c5c7136d
olszakjakub <jakub.olszak2@gmail.com>
2021-09-19 15:17:30 +0200

    added image upload and processing, working on video processing

--- server/server.py
+++ server/server.py
@@ -1,16 +1,22 @@
 from os import name
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.responses import HTMLResponse, JSONResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 import json
 from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
 from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
+from starlette.responses import StreamingResponse
 import uuid
 import logging
 import cv2
 import asyncio
-import os
+import io
+import numpy as np
+import base64
+import tensorflow as tf
+from tensorflow import keras
 
 app = FastAPI()
 
@@ -20,6 +26,28 @@ logger = logging.getLogger("pc")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
+
+# Loading model trained earlier
+model = keras.models.load_model('saved_model')
+
+# Font for text on video
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+
+# Cascade for detecting faces
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+origins = [
+    'http://localhost',
+    'http://127.0.0.1'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class VideoTransformTrack(MediaStreamTrack):
 
@@ -32,7 +60,7 @@ class VideoTransformTrack(MediaStreamTrack):
     async def recv(self):
         
         frame = await self.track.recv()
-
+        print(frame)
         return frame
 
 @app.get('/favicon.ico')
@@ -105,3 +133,89 @@ async def on_shutdown():
     coros = [pc.close() for pc in pcs]
     await asyncio.gather(*coros)
     pcs.clear()
+
+@app.post("/photovideo")
+async def image(file: UploadFile = File(...)):
+
+    contents = await file.read()
+    nparr = np.fromstring(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    img_dimensions = str(img.shape)
+
+    processed_img = processFrame(img)
+
+    _, encoded_img = cv2.imencode('.png', processed_img)
+    encoded_img = base64.b64encode(encoded_img)
+
+    return {
+        'filename': file.filename,
+        'img_dimensions': img_dimensions,
+        'encoded_img': encoded_img,
+    }
+
+def processFrame(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detecting faces using haarcascade
+    faces = face_cascade.detectMultiScale(gray,
+                                          scaleFactor=1.1,
+                                          minNeighbors=5,
+                                          minSize=(60, 60),
+                                          flags=cv2.CASCADE_SCALE_IMAGE)
+    
+    # For every detected face predictions are made to determine if face
+    # has facemask on it or not. If it has facemask on then green rectangle
+    # is drawn and if not red one.
+    for (x, y, w, h) in faces:
+
+        # Checking if values are not exceeding dimensions of the frame
+        if (y-40) > 0:
+            y0 = y-40    
+        else:
+            y0 = 0
+        
+        if (y+h+40) < frame.shape[0]:
+            y1 = y+h+40  
+        else:
+            y1 = frame.shape[0]
+
+        if (x-20) > 0:
+            x0 = x-20
+        else:
+            x0 = 0
+
+        if (x+w+20) < frame.shape[1]:
+            x1 = x+w+20 
+        else:
+            x1 = frame.shape[1]
+
+        # Preparing detected face to be predicted by the model:
+        # - we're cutting face from the frame
+        # - then we're changing color space from BGR to RGB
+        # - and on the end we're resizing it to 140 x 140 (model is prepared for such data)
+        img_array = keras.preprocessing.image.img_to_array(cv2.resize(cv2.cvtColor(frame[y0:y1, x0:x1], cv2.COLOR_BGR2RGB),(140,140)))
+        img_array = tf.expand_dims(img_array, 0)
+        
+        # Checking the image
+        predictions = model.predict(img_array)
+        
+        # Getting the score
+        score = float(predictions[0])
+
+        # Preparing text for the image
+        predictions_text_1 = "{:.2f}% with mask".format(100 * (1 - score))
+        predictions_text_2 = "{:.2f}% without mask".format(100 * score)
+
+        # Checking if prediction for wearing a mask are more than 50%
+        # If so green rectangle will be drawn and if not red one
+        if score < 0.5:
+            cv2.rectangle(frame, (x-20, y-40), (x+w+20, y+h+40), (0,255,0), 2)
+        else:
+            cv2.rectangle(frame, (x-20, y-40), (x+w+20, y+h+40), (0,0,255), 2)
+        
+        # Adding text with score of prediction
+        cv2.putText(frame, predictions_text_1, (x, y+h+50), font, 1,(255,255,255),2,cv2.LINE_AA)
+        cv2.putText(frame, predictions_text_2, (x, y+h+80), font, 1,(255,255,255),2,cv2.LINE_AA)
+
+        return frame
--- server/static/main.js
+++ server/static/main.js
@@ -3,6 +3,8 @@ var pc = null;
 
 var webcamContainer = document.getElementById("webcam-container");
 var photoVideoContainer = document.getElementById("photovideo-container");
+var photovideoBtn = document.getElementById("photovideoBtn");
+var cameraBtn = document.getElementById("cameraBtn");
 
 function createPeerConnection() {
     
@@ -63,16 +65,19 @@ function negotiate() {
 }
 
 function start() {
-    document.getElementById('start').style.display = 'none';
+    document.getElementById('start').hidden = true;
 
     pc = createPeerConnection();
 
     var constraints = {
         audio: false,
-        video: true
+        video: {
+            width: { min: 640, ideal: 1280, max: 1280 },
+            height: { min: 480, ideal: 720, max: 720 }
+        }
     }
 
-    document.getElementById('video').style.display = 'block';
+    document.getElementById('video').hidden = false;
 
     navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
         stream.getTracks().forEach(function(track) {
@@ -83,11 +88,11 @@ function start() {
         alert('Could not acquire media: ' + err);
     });
 
-    document.getElementById('stop').style.display = 'inline-block';
+    document.getElementById('stop').hidden = false;
 }
 
 function stop() {
-    document.getElementById('stop').style.display = 'none';
+    document.getElementById('stop').hidden = true;
 
     if(pc.getTransceivers) {
         pc.getTransceivers().forEach(function(transceiver) {
@@ -105,20 +110,77 @@ function stop() {
         pc.close();
     }, 500);
 
-    document.getElementById('start').style.display = 'inline-block';
-    document.getElementById('video').style.display = 'none';
+    document.getElementById('start').hidden = false;
+    document.getElementById('video').hidden = true;
 }
 
 function webcamClick() {
-    if (webcamContainer.style.display == 'none') {
+    if (webcamContainer.hidden == true) {
         photoVideoContainer.style.display = 'none';
-        webcamContainer.style.display = 'block';
+        webcamContainer.hidden = false;
+        cameraBtn.style.textDecoration = "underline"
+        cameraBtn.style.fontWeight = "bold"
+        photovideoBtn.style.textDecoration = "none"
+        photovideoBtn.style.fontWeight = "normal"
     }
 }
 
 function photoVideoClick() {
     if (photoVideoContainer.style.display == 'none') {
-        webcamContainer.style.display = 'none';
+        webcamContainer.hidden = true;
         photoVideoContainer.style.display = 'block';
+        cameraBtn.style.textDecoration = "none"
+        cameraBtn.style.fontWeight = "normal"
+        photovideoBtn.style.textDecoration = "underline"
+        photovideoBtn.style.fontWeight = "bold"
     }
 }
+
+$(document).ready(function(){
+
+    $("#but_upload").click(function(){
+
+        var fd = new FormData();
+        var files = $('#file')[0].files;
+        
+
+        if(files.length > 0 ){
+        fd.append('file',files[0]);
+
+        $.ajax({
+            url: 'http://127.0.0.1:8000/photovideo',
+            type: 'post',
+            data: fd,
+            contentType: false,
+            processData: false,
+            success: function(response){
+             if(response != 0){
+                
+                console.log(response)
+
+                $("#img").attr('src',`data:image/png;base64,${response["encoded_img"]}`);
+                // let height = response["img_dimensions"].substring(1, response["img_dimensions"].length-1).split(',')[0];
+                // let width = response["img_dimensions"].substring(1, response["img_dimensions"].length-1).split(',')[1];
+                // console.log(width)
+                // $("#img").height(height).width(width);
+                
+                // if(response["suffix"] == "mp4") {
+                //     $("#my-video source").attr("src","../backend/Images/"+response["name"]+"."+response["suffix"]);
+                //     $("#my-video")[0].load()
+                //     $("#my-video").show();
+                // } else {
+                //     $("#img").attr("src","../backend/Images/"+response["name"]+"."+response["suffix"]); 
+                //     $(".preview img").show();
+                // }
+
+
+             }else{
+                alert('file not uploaded');
+             }
+          },
+        });
+        } else {
+            alert("Please select a file.");
+        }
+    });
+});
